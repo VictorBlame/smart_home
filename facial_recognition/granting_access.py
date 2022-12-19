8a448f17c5354b6ba94907b13fc3f8339dfeaae2
VictorBlame <basketball161616@gmail.com>
2022-12-19 23:51:43 +0100

    implementing logger + starting image analyze

--- facial_recognition/granting_access.py
+++ facial_recognition/granting_access.py
@@ -2,6 +2,16 @@ import cv2
 import face_recognition
 import sys
 import os
+import logging
+
+
+logging.basicConfig(
+    filename='logs/runLog.log',
+    level=logging.DEBUG,
+    format='%(asctime)s.%(msecs)03d | %(levelname)8s | %(message)s',
+    datefmt='%Y-%m-%d %H:%M:%S',
+)
+log = logging.getLogger(__name__)
 
 
 def take_picture():
@@ -12,6 +22,11 @@ def take_picture():
     cv2.destroyAllWindows()
     cap.release()
     print('Face scan complete')
+    log.info('Face scan complete')
+
+
+def analyze_login_user():
+    base_img = cv2.cvtColor(face_recognition.load_image_file('authenticated_users/Roland_Mayer.jpg'), cv2.COLOR_BGR2RGB)
 
 
 take_picture()
