c924b9fc66501d960e2f5ae82c1824c1f76da3d2
VictorBlame <basketball161616@gmail.com>
2022-12-22 16:35:15 +0100

    bugfix when storing multiple users part 1, known issues: -double checking on users -scanning works if every known user has same amount of images

--- facial_recognition/granting_access.py
+++ facial_recognition/granting_access.py
@@ -3,6 +3,7 @@ import face_recognition
 import os
 import logging
 import datetime
+import numpy as np
 
 
 KNOWN_FACES_DIR = 'authenticated_users'
@@ -18,8 +19,6 @@ IMAGE_RESIZE_PERCENTAGE = 20 #smallest value is 20
 GREEN = '\x1b[1;32;40m'
 RED = '\x1b[1;31;40m'
 END = '\x1b[0m'
-ACCESS_GRANTED = 'ACCESS_GRANTED'
-ACCESS_DENIED = 'ACCESS_DENIED'
 
 logging.basicConfig(
     filename=f'{LOGS_DIR}/face_authenticator.log',
@@ -86,7 +85,8 @@ def loading_authenticated_users(array_of_faces, array_of_names):
                 encoding = face_recognition.face_encodings(resized_image)[0]
 
                 array_of_faces.append(encoding)
-                array_of_names.append(name)
+                if name not in array_of_names:
+                    array_of_names.append(name)
         log.info('Known faces loading was successful')
     except Exception as ex:
         error_message = 'Something went wrong with loading_authenticated_users function. The problem was: ' + str(ex)
@@ -96,14 +96,14 @@ def loading_authenticated_users(array_of_faces, array_of_names):
 
 def processing_unknown_users(array_of_faces, tolerance):
     try:
-        for filename in os.listdir(UNKNOWN_FACES_DIR):
+        for filename, number_of_users in zip(os.listdir(UNKNOWN_FACES_DIR), range(1, len(os.listdir(UNKNOWN_FACES_DIR)) + 1)):
             print(f'Filename {filename}', end='')
             log.info('------------ Processing ' + str(filename) + ' has started ------------')
             image_original = cv2.cvtColor(face_recognition.load_image_file(f'{UNKNOWN_FACES_DIR}/{filename}'), cv2.COLOR_RGB2BGR)
             image_resized = resize_image_by_percentage(image_original, IMAGE_RESIZE_PERCENTAGE)
             locations = face_recognition.face_locations(image_resized, model=MODEL)
             encodings = face_recognition.face_encodings(image_resized, locations)
-            showing_granted_images(encodings, locations, array_of_faces, tolerance, image_original)
+            showing_granted_images(encodings, locations, array_of_faces, tolerance, image_original, number_of_users)
             log.info('------------ Processing ' + str(filename) + ' was successful ------------')
         message = 'Everything was okay with processing_unknown_users function'
         log.info(message)
@@ -113,15 +113,16 @@ def processing_unknown_users(array_of_faces, tolerance):
         log.error(error_message)
 
 
-def showing_granted_images(encodings, locations, known_faces, tolerance, image_original):
+def showing_granted_images(encodings, locations, known_faces, tolerance, image_original, number_of_users):
     try:
         #take_picture()
         print(f', found {len(encodings)} face(s)')
         for face_encoding, face_location in zip(encodings, locations):
+            print(number_of_users)
             results = face_recognition.compare_faces(known_faces, face_encoding, tolerance)
             access_granted = minimum_success_rate(ACCEPTANCE_PERCENTAGE, results)
             if access_granted:
-                match = known_names[results.index(True)] + ' ' + GREEN + 'ACCESS GRANTED' + END
+                match = known_names[number_of_users - 1] + ' ' + GREEN + 'ACCESS GRANTED' + END
                 log_text = known_names[results.index(True)] + ' ACCESS GRANTED'
                 time_string = datetime.datetime.now().strftime("%Y_%m_%d_%H_%M_%S") + '_ACCESS_GRANTED'
                 log.info(log_text)
@@ -141,10 +142,16 @@ def showing_granted_images(encodings, locations, known_faces, tolerance, image_o
         log.error(error_message)
 
 
+def split_list_run(known_faces, known_names, tolerance):
+    for process in np.array_split(known_faces, len(known_names)):
+        processing_unknown_users(process, tolerance)
+
+
 print('Loading known faces...')
 known_faces = []
 known_names = []
 
+
 loading_authenticated_users(known_faces, known_names)
-processing_unknown_users(known_faces, TOLERANCE)
+split_list_run(known_faces, known_names, TOLERANCE)
 log.debug('AUTHENTICATION SCRIPT END')
