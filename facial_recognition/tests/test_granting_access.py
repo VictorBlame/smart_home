3da9d1c00940c2927f6235e8ffa13be1db251beb
EPAM\Roland_Mayer <Roland_Mayer@epam.com>
2023-07-04 17:22:21 +0200

    test minimum_success_rate

--- facial_recognition/tests/test_granting_access.py
+++ facial_recognition/tests/test_granting_access.py
@@ -1,7 +1,14 @@
 import facial_recognition.src.config as conf
-from facial_recognition.src.granting_access import resize_image_by_percentage
+from facial_recognition.src.granting_access import resize_image_by_percentage, minimum_success_rate
 
 
 def test_resize_image_by_percentage(input_image):
     resized_image = resize_image_by_percentage(input_image, conf.IMAGE_RESIZE_PERCENTAGE)
     assert int(resized_image.shape[0]) == int(input_image.shape[0] * (conf.IMAGE_RESIZE_PERCENTAGE / 100))
+
+
+def test_minimum_success_rate():
+    success = minimum_success_rate(55, [True, True, False, True, True])
+    fail = minimum_success_rate(55, [True, False, False, False, True])
+    assert success == True
+    assert fail == False
